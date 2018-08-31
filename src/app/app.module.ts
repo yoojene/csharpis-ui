968b808928ba7488456d9151804797e6c3c90768
Jasonhughes94 <jason.hughes@arnoldclark.com>
2018-08-31 21:01:01 +0100

    Add Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 import { AppComponent } from './app.component';
+import { SearchRetrieverService } from './services/search-retriever.service';
+import { AboutUsComponent } from './components/about-us/about-us.component';
+
+const appRoutes = [
+  { path: '', component: AppComponent },
+  { path: 'about-us', component: AboutUsComponent },
+  { path: '**', redirectTo: '' }
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutUsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes),
   ],
-  providers: [],
+  providers: [
+    SearchRetrieverService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
