d54e0e55d1190c9721d514b73b675ad4597b0451
Jasonhughes94 <jason.hughes@arnoldclark.com>
2018-08-31 21:01:24 +0100

    Add basic search retriever service

---
+++ src/app/models/search.ts
@@ -0,0 +1,4 @@
+export class Search {
+  values: any;
+  pages: number;
+}
---
+++ src/app/services/search-retriever.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { Search } from '../models/search';
+import { environment } from '../../environments/environment.prod';
+
+@Injectable()
+export class SearchRetrieverService {
+  private apiUrl = environment.Api.searchRetriever;
+
+  constructor(private _httpClient: HttpClient) {}
+
+  retrieve(query: string, page: number): Observable<Search> {
+    return this._httpClient
+      .get(`${this.apiUrl}?query=${query}&page=${page}`)
+      .pipe(catchError(error => this.handleError(error)));
+  }
+
+  private handleError(error: any): Promise<any> {
+    console.error('An error occurred', error);
+    return Promise.reject(error.message || error);
+  }
+}
