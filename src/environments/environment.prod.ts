d2eb00fdbd3a9ac7c6517a011d54c79cbd2e420f
Jasonhughes94 <jason.hughes@arnoldclark.com>
2018-09-01 16:40:08 +0100

    Update environment prod - remove redundant values

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   Api: {
-    searchRetriever: 'https://csharp.is/api/search?query=finite&page=1'
+    searchRetriever: 'https://csharp.is/api/search'
   }
 };
