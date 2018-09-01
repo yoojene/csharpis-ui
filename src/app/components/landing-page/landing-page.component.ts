e792f6e93a7c0d828908ea99658d5f1b8c54e28f
Jasonhughes94 <jason.hughes@arnoldclark.com>
2018-09-01 14:15:26 +0100

    Add Basic landing page styles - Add form submission

--- src/app/components/landing-page/landing-page.component.ts
+++ src/app/components/landing-page/landing-page.component.ts
@@ -1,11 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { Search } from '../../models/search';
+import { SearchRetrieverService } from '../../services/search-retriever.service';
 
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.scss']
 })
-export class LandingPageComponent {
+export class LandingPageComponent implements OnInit {
 
+  csSearchFormGroup: FormGroup;
+  searchResults: Search[];
+  error: boolean;
+  reultsFound: boolean;
+
+  constructor(
+    private _formBuilder: FormBuilder,
+    private _searchRetriever: SearchRetrieverService
+  ) {}
+
+  ngOnInit(): void {
+    this.buildForm();
+  }
+
+  buildForm(): void {
+    this.csSearchFormGroup = this._formBuilder.group({
+      value: ''
+    });
+  }
+
+  submitForm(): void {
+    console.log(this.csSearchFormGroup.value);
+    this._searchRetriever.retrieve(this.csSearchFormGroup.value).subscribe(result => {
+      this.searchResults = result;
+    }, error => {
+      console.log(error);
+      this.error = true;
+    });
+  }
 }
 
