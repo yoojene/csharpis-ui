3a216662138ad306864c610b121d58ac4f39639a
Jasonhughes94 <jason.hughes@arnoldclark.com>
2018-09-06 20:47:47 +0100

    Use the correct port

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   Api: {
-    searchRetriever: 'https://localhost:5001/api/search'
+    searchRetriever: 'https://localhost:44380/api/search'
   }
 };
