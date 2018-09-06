4f05d2a3debd044ec2f28eedc09473f555823df6
Jasonhughes94 <jason.hughes@arnoldclark.com>
2018-09-06 20:48:09 +0100

    Update the model to reflect the the JSON the API returns

--- src/app/models/search.ts
+++ src/app/models/search.ts
@@ -1,8 +1,13 @@
 export class Search {
+  id: number;
   url: string;
   name: string;
+  slug: string;
   description: string;
-  project: string;
-  author: string;
+  authorId: number;
+  authorName: string;
+  imageUrl: string;
+  dateAdded: Date;
+  dateLastUpdated: Date;
   tags: string[];
 }
